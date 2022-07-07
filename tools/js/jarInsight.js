5ef090684de2ee93ab9b53b74082667561391a09
超神的冰凉 <2801621420@qq.com>
2022-07-07 17:42:52 +0800

    jarInsight UI优化

--- tools/js/jarInsight.js
+++ tools/js/jarInsight.js
@@ -1,3 +1,90 @@
+// noinspection JSUnresolvedVariable,DuplicatedCode
+
+let languageId = (window.language || navigator.language || navigator.browserLanguage).toLowerCase();
+const $ = mdui.$;
+
+const eng = {
+    "wip.main": "Developing...",
+    "wip.ok": "OK",
+    "more-menu.switch-theme": "Switch theme",
+    "more-menu.switch-to-day": "Switched to day theme",
+    "more-menu.switch-to-night": "Switched to night theme",
+    "more-menu.ok": "OK",
+    "title": "PNX Jar Insight",
+    "text.processed": "Processed: ",
+    "text.classes": "classes",
+    "text.methods": "methods",
+    "text.fields": "fields",
+    "text.display": "Display d.ts file",
+    "text.save": "Save d.ts file"
+}
+
+const chn = {
+    "wip.main": "此功能正在开发中",
+    "wip.ok": "确认",
+    "more-menu.switch-theme": "切换主题",
+    "more-menu.switch-to-day": "已切换至日间主题",
+    "more-menu.switch-to-night": "已切换至夜间主题",
+    "more-menu.ok": "确认",
+    "title": "PNX字节码分析工具",
+    "text.processed": "已处理：",
+    "text.classes": "个类",
+    "text.methods": "个方法",
+    "text.fields": "个字段",
+    "text.display": "显示 d.ts 定义文件",
+    "text.save": "保存 d.ts 定义文件"
+}
+
+/**
+ *
+ * @param key {string}
+ * @return {string}
+ */
+function translate(key) {
+    if (languageId === "zh-cn") {
+        const tmp = chn[key];
+        return tmp ? tmp : key;
+    }
+    const tmp = eng[key];
+    return tmp ? tmp : key;
+}
+
+$('[translate=yes]').each((index, value) => {
+    $(value).text(translate($(value).text()))
+});
+
+function switchTheme(noNotice) {
+    const clazz = document.body.classList;
+    if (clazz.contains("mdui-theme-layout-dark")) {
+        clazz.remove("mdui-theme-layout-dark", "mdui-theme-primary-light-blue", "mdui-theme-accent-light-blue");
+        clazz.add("mdui-theme-primary-indigo", "mdui-theme-accent-indigo");
+        if (!noNotice) {
+            mdui.snackbar(translate("more-menu.switch-to-day"), {
+                buttonText: translate("more-menu.ok")
+            });
+        }
+        window.localStorage.setItem("dark-theme", "false");
+    } else {
+        clazz.remove("mdui-theme-primary-indigo", "mdui-theme-accent-indigo");
+        clazz.add("mdui-theme-layout-dark", "mdui-theme-primary-light-blue", "mdui-theme-accent-light-blue");
+        if (!noNotice) {
+            mdui.snackbar(translate("more-menu.switch-to-night"), {
+                buttonText: translate("more-menu.ok")
+            })
+        }
+        window.localStorage.setItem("dark-theme", "true");
+    }
+}
+
+function defaultCheckTheme() {
+    const flag = window.localStorage.getItem("dark-theme");
+    if (flag && flag === "true") {
+        switchTheme(true);
+    }
+}
+
+defaultCheckTheme();
+
 const baseJavaDts = `declare type byte = number;
 declare type char = number;
 declare type short = number;
